6fe97f0cb4fca1a06210edd88910ae6e76723bd7
Dmprogram <dmprogramhh@mail.ru>
2022-03-24 15:08:20 +0300

    getting-api-data

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-rolling-nstsp2mnos">
+            <div className="ldio-ksenekrpj6h">
+                <div></div>   
+            </div>
+        </div>
+                    
+    );
+};
+
+export default Spinner;
